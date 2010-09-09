58ddc0b0800a57212284135126c7356015655e12
Nathaniel Flath <flat0103@gmail.com>
2010-09-09 10:42:11 -0700

    Auto commit from emacs

--- scratch.el
+++ scratch.el
@@ -110,3 +110,6 @@ URL in a new window."
 
 (define-key c-mode-map (kbd "C-M-a") 'ido-beginning-of-defun)
 (define-key c++-mode-map (kbd "C-M-a") 'ido-beginning-of-defun)
+
+(define-key c-mode-map (kbd "C-a") 'nflath-cycle-bol)
+(define-key c++-mode-map (kbd "C-a") 'nflath-cycle-bol)
