f1aba8773cdc36ac3ef3808778f1b21b535c16cb
Nathaniel Flath <flat0103@gmail.com>
2010-09-09 10:31:25 -0700

    Auto commit from emacs

--- internal/programming.el
+++ internal/programming.el
@@ -76,6 +76,44 @@
               (goto-char position) (goto-char (overlay-start position))))))))
 (global-set-key (kbd "M-s") 'ido-goto-symbol)
 
+(defun ido-beginning-of-defun ()
+  "Will update the imenu index and then use ido to select a symbol to navigate to."
+  (interactive)
+  (setq imenu-create-index-function 'imenu-default-create-index-function)
+  (imenu--make-index-alist)
+  (let ((name-and-pos '())
+        (symbol-names '()))
+    (flet ((addsymbols (symbol-list)
+                       (when (listp symbol-list)
+                         (dolist (symbol symbol-list)
+                           (let ((name nil) (position nil))
+                             (cond
+                              ((and (listp symbol) (imenu--subalist-p symbol))
+                               (addsymbols symbol))
+                              ((listp symbol)
+                               (setq name (car symbol))
+                               (setq position (cdr symbol)))
+                              ((stringp symbol)
+                               (setq name symbol)
+                               (setq position (get-text-property 1 'org-imenu-marker symbol))))
+                             (unless (or (null position) (null name))
+                               (add-to-list 'symbol-names name)
+                               (add-to-list 'name-and-pos (cons name position))))))))
+      (addsymbols imenu--index-alist)
+      (if (not symbol-names)
+          (ido-goto-symbol)
+        (let* ((symbol-at-point (symbol-name (symbol-at-point)))
+               (selected-symbol (ido-completing-read
+                                 "Symbol? "
+                                 (if (member symbol-at-point symbol-names)
+                                     (cons symbol-at-point (remove-if
+                                                            (lambda (x) (string-equal x symbol-at-point))
+                                                            symbol-names))
+                                   symbol-names)))
+               (position (cdr (assoc selected-symbol name-and-pos))))
+          (if (markerp position)
+              (goto-char position) (goto-char (overlay-start position))))))))
+
 ;;Compiling always asks to save my buffers; I want it to instead do nothing, so I have save-some-buffers only do stuff if passed a filter.
 (defadvice save-some-buffers (around save-buffers-ignore activate)
   (if (ad-get-arg 1)
