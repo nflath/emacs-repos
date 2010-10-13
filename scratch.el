2c97a0606bf3431dc80f3f0bf5c084b1f76c18df
Nathaniel Flath <flat0103@gmail.com>
2010-10-13 15:12:06 -0700

    Auto commit from emacs

--- scratch.el
+++ scratch.el
@@ -1,8 +1,13 @@
 ;; This buffer is for notes you don't want to save, and for Lisp evaluation.  If you want to create a file, visit that
 ;; file with C-x C-f, then enter the text in that file's own buffer.
 
+(setq command-list
+      '( "d" . 'kill-region))
 
+(setq movement-list
+      '( "f" . 'forward-word))
 
-
-
-
+(defun perform-command-movement ()
+  (interactive)
+  
+  )
