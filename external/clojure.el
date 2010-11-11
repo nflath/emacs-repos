1783c1b4019c05dc88cbbec4881d3f53ec5231e0
Nathaniel Flath <flat0103@gmail.com>
2010-11-11 10:08:13 -0800

    set up clojure

--- external/clojure.el
+++ external/clojure.el
@@ -1,8 +1,7 @@
-(setq clojure-src-root "~/.emacs.d/")
-(setq package-activated-list nil)
 (require 'clojure-mode)
 
-;;Start a REPL
-(when (file-exists-p "/home/nflath/.emacs.d/clojure.jar")
-    (setq inferior-lisp-program "java -cp /home/nflath/.emacs.d/clojure.jar clojure.main")
-    (inferior-lisp inferior-lisp-program))
+(eval-after-load "slime"
+  '(progn (slime-setup '(slime-repl))))
+
+(require 'slime)
+(slime-setup)
