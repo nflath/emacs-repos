f826e6430303f5d0e7d6cf2e0c9ff3598174634e
unknown <nflath@.(none)>
2010-11-15 18:24:57 -0800

    Removed duplicate functionality

--- internal/cc.el
+++ internal/cc.el
@@ -13,5 +13,25 @@
           (insert "+ \""))
       (newline-and-indent))))
 
-(define-key c-mode-map (kbd "C-M-a") 'ido-beginning-of-defun)
-(define-key c++-mode-map (kbd "C-M-a") 'ido-beginning-of-defun)
+(defun c-least-enclosing-brace (paren-state)
+  ;; Return the bufpos of the outermost enclosing open paren, or nil
+  ;; if none was found.
+  (save-excursion
+    (let (pos elem)
+      (while paren-state
+        (setq elem (car paren-state)
+              paren-state (cdr paren-state))
+        (when (integerp elem)
+          (setq pos elem)
+          (goto-char pos)
+          (c-skip-ws-backward)
+          (let ((syntax (c-guess-basic-syntax)))
+            (while syntax
+              (when (eq (caar syntax) 'topmost-intro)
+                (setq syntax nil)
+                (setq paren-state nil))
+              (if syntax (setq syntax (cdr syntax)))))))
+      pos)))
+
+(define-key c-mode-map (kbd "C-M-a") 'c-beginning-of-defun)
+(define-key c++-mode-map (kbd "C-M-a") 'c-beginning-of-defun)
