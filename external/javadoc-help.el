68d7d2ac6b0d46e491182a0207d835e0b8e85486
Nathaniel Flath <flat0103@gmail.com>
2011-02-27 06:52:04 -0500

    Have internet connectivity not fail emacs startup on javadoc-help

--- external/javadoc-help.el
+++ external/javadoc-help.el
@@ -4,6 +4,8 @@
 (javadoc-set-predefined-urls '("http://download.oracle.com/javase/1.5.0/docs/api/"))
 
 (jdh-process-predefined-urls *jdh-predefined-urls*)
-(jdh-refresh-url "http://download.oracle.com/javase/1.5.0/docs/api/")
+(condition-case nil
+    (jdh-refresh-url "http://download.oracle.com/javase/1.5.0/docs/api/")
+  (error nil))
 
 (define-key java-mode-map (kbd "C-x j") 'javadoc-lookup)
