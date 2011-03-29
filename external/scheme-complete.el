b928fd7505c2545894791a54610d10193411a7f5
Nathaniel Flath <flat0103@gmail.com>
2011-03-29 10:37:34 -0400

    scheme-complete enabled

--- external/scheme-complete.el
+++ external/scheme-complete.el
@@ -1,9 +1,9 @@
 (require 'scheme)
-;;(require 'scheme-complete)
+(require 'scheme-complete)
 (define-key scheme-mode-map "\t" 'scheme-complete-or-indent)
-;;(autoload 'scheme-get-current-symbol-info "scheme-complete" nil t)
-;; (add-hook 'scheme-mode-hook
-;;   (lambda ()
-;;     (make-local-variable 'eldoc-documentation-function)
-;;     (setq eldoc-documentation-function 'scheme-get-current-symbol-info)
-;;     (eldoc-mode)))
+(autoload 'scheme-get-current-symbol-info "scheme-complete" nil t)
+(add-hook 'scheme-mode-hook
+          (lambda ()
+            (make-local-variable 'eldoc-documentation-function)
+            (setq eldoc-documentation-function 'scheme-get-current-symbol-info)
+            (eldoc-mode)))
