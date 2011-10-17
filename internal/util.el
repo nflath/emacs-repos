1273563b9592265f2597e5b0d241415ec54bccba
unknown <nflath@.(none)>
2011-10-16 22:52:30 -0700

    go through optum stuff

--- internal/util.el
+++ internal/util.el
@@ -369,3 +369,26 @@ file of a buffer in an external program."
       (set-window-start w2 s1)))
   (other-window 1))
 
+(defun add (amt) 
+  "Increment the number (in base 10 representation) at point."
+  (interactive "nAmount to add: ")
+  (if (looking-at "[-0-9.]+")
+      (progn
+        (let ((num (car (read-from-string (buffer-substring (match-beginning 0) (match-end 0))))))
+          (delete-region (match-beginning 0) (match-end 0))
+          (setq num (+ num amt))
+          (insert (format (if (< (abs (- num (round num))) 0.001) "%.0f" "%.2f") num))))))
+
+(defun ++ ()
+  "Increment the number (in base 10 representation) at point."
+  (interactive)
+  (add 1))
+
+(defun mul (amt) 
+  "Multiply the number (in base 10 representation) at point."
+  (interactive "nAmount to multiply by: ")
+  (if (looking-at "[-0-9.]+")
+      (progn
+        (let ((num (car (read-from-string (buffer-substring (match-beginning 0) (match-end 0))))))
+          (delete-region (match-beginning 0) (match-end 0))
+          (insert (format "%f" (* num amt)))))))
