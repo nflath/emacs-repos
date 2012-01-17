4eed98f2386880cc8cc6a75a139ff23db0ba3ba5
Nathaniel Flath <flat0103@gmail.com>
2012-01-17 13:36:39 -0800

    Get ird of python stuff

--- internal/python.el
+++ internal/python.el
@@ -21,40 +21,6 @@
 (when (= 0 (shell-command "pyflakes"))
   (setq flymake-enable-pyflakes t)
   (setq flymake-enable-pylint nil)
-  (setq flymake-enable-pep8 nil)
-
-  (eval-after-load 'python
-    '(progn
-       ;; Init functions!
-       (defun flymake-pyflakes-init ()
-         (flymake-command-setup "pyflakes"))
-
-       (defun flymake-pep8-init ()
-         (flymake-command-setup "pep8"))
-
-       (defun flymake-pylint-init ()
-         (flymake-command-setup "python" (list (concat emacs-repos-dir "/pylint-mod.py"))))
-
-       (defun flymake-disable-python-checkers ()
-         "Disable all python checkers"
-         (dolist (flymake-checker-init '(flymake-pyflakes-init flymake-pep8-init flymake-pylint-init))
-           (remove '("\\.py\\'" flymake-checker-init) 'flymake-allowed-file-name-masks)))
-
-       (defun flymake-add-checker (command)
-         "Add the checker specified by the COMMAND list"
-         (add-to-list 'flymake-allowed-file-name-masks
-                      (list "\\.py\\'" command)))
-
-       ;; Not on all modes, please
-       (add-hook 'python-mode-hook 'flymake-find-file-hook)
-
-       (when flymake-enable-pyflakes
-         (flymake-add-checker 'flymake-pyflakes-init))
-
-       (when flymake-enable-pylint
-         (flymake-add-checker 'flymake-pylint-init))
-
-       (when flymake-enable-pep8
-         (flymake-add-checker 'flymake-pep8-init)))))
+  (setq flymake-enable-pep8 nil))
 
 (setq python-remove-cwd-from-path nil)
