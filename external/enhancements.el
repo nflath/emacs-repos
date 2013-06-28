82d0b6a035d203df4be2d13bf7c273c62a403baf
Nathaniel Flath <flat0103@gmail.com>
2013-06-27 19:44:47 -0700

    Remove unecesasry adds to auto-mode-alist

--- external/enhancements.el
+++ external/enhancements.el
@@ -1,12 +1,8 @@
 (setq wgrep-enable-key "q")
 ;; FixMe: Add this to mv-shell.el
 (mv-shell-mode 1)
-(add-hook 'shell-mode-hook 'gdb-shell-minor-mode)
-(paren-activate)
-
+o
 (eval-after-load 'init-finished
   '(progn
      (setq save-visited-files-auto-restore t)
      (save-visited-files-mode t)))
-
-(add-hook 'org-mode 'org-bullets-mode)
--- external/packages.el
+++ external/packages.el
@@ -1,10 +1,7 @@
 ;;; Customization for package.el packages that don't require a full file of their own
 
 ;;; Major modes
-;; FixMe: Are these necessary?
 (add-to-list 'auto-mode-alist '("\\.yml" .yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 
 (setq markdown-enable-math t)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
